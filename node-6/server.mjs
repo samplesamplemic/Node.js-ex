a1cf19a812015056976f8e2648c5166453ad4c63
Mic <michelelt660@gmail.com>
2022-10-04 14:56:59 +0200

    ex-06

---
+++ node-6/server.mjs
@@ -0,0 +1,20 @@
+import { createServer } from "node:http";
+
+const server = createServer((request, response) => {
+  console.log("request received");
+
+  response.statusCode = 200;
+
+  response.setHeader("Content-Type", "application/json");
+
+  const jsonResponseBody = JSON.stringify({ location: "Mars" });
+
+  response.end(jsonResponseBody);
+});
+
+server.listen(3000, () => {
+  console.log(`Server running at http://localhost:3000`);
+});
+
+//QUESTION: What is the value of the Content-Length response header?
+//ANSWER: Content-Length: 19
