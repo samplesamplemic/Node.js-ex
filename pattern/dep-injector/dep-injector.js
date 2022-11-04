29e9267065005a2248a389ba95362bc2d850b4fe
Mic <michelelt660@gmail.com>
2022-11-04 20:48:24 +0100

    dep-injector pattern

---
+++ pattern/dep-injector/dep-injector.js
@@ -0,0 +1,29 @@
+class Bike {
+  constructor() {
+    this.wheel1 = new Wheel("Front", 126);
+    this.wheel2 = new Wheel("Back", 42);
+  }
+
+  specification() {
+    let message = `${this.wheel1.label} wheel diameter = ${this.wheel1.diameter}`;
+    message += `, ${this.wheel2.label} wheel diameter = ${this.wheel2.diameter}`;
+
+    return message;
+  }
+}
+
+class Wheel {
+  constructor(label, diameter) {
+    this.label = label;
+    this.diameter = diameter;
+  }
+}
+
+// const frontWheel = new Wheel("Front", 126);
+// const backWheel = new Wheel("Back", 42);
+
+const bike = new Bike();
+
+console.log(bike);
+
+console.log("Bike specification:", bike.specification());
